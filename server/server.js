1fa824b564ab380aee23e33be46f1b0e57fe8314
Karan Keswani <karankeswani99@gmail.com>
2019-01-01 18:30:38 +0530

    Add geolocation support via geolocation API

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port              = process.env.PORT || 3000;
 const socketIO          = require('socket.io');
 var server              = http.createServer(app);
 var io                  = socketIO(server);//what we get back is our web socket server
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 //Configuring your middleware
 app.use(express.static(publicPath));
 
@@ -28,11 +28,11 @@ io.on('connection', (socket) => {
 		console.log('There is a new Message', message);
 		io.emit('newMessage', generateMessage(message.from,message.text));
 		callback('This is from the server');
-		// socket.broadcast.emit('newMessage', {
-		// 	from: message.from,
-		// 	text: message.text,
-		// 	createdAt: new Date().getTime()
-		// });
+		
+	});
+
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
 	});
 
 	socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect            = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
@@ -9,5 +9,17 @@ describe('generateMessage', () => {
 		expect(res.from).toBe(from);
 		expect(res.text).toBe(text);
 		expect(res.createdAt).toBeA('number');
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct Location Object', () => {
+		var from = 'Karan';
+		var latitude = 1;
+		var longitude = 1;
+		var result = generateLocationMessage(from, latitude, longitude);
+		expect(result.from).toBe(from);
+		expect(result.url).toBe('https://www.google.com/maps?q=1,1');
+		expect(result.createdAt).toBeA('number');
 	});
 });
